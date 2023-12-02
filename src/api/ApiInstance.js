af43518b9827fcef74c703088e24702a276f29c5
nickgendron <52512014+nickgendron@users.noreply.github.com>
2023-12-02 17:35:33 -0600

    configured azure app service

--- src/api/ApiInstance.js
+++ src/api/ApiInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = "http://localhost:5093";
+const baseUrl = "https://kiblbackendfinal.azurewebsites.net";
 export const kanbanApi = axios.create({
   baseURL: baseUrl,
   withCredentials: true,
