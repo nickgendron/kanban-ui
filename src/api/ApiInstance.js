a92cb95b66d08ec50f4d6639e78fd60c49d828a9
Nicholas Gendron <52512014+nickgendron@users.noreply.github.com>
2023-12-02 21:40:36 -0600

    Update ApiInstance.js

--- src/api/ApiInstance.js
+++ src/api/ApiInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = "https://kiblbackendfinal.azurewebsites.net";
+const baseUrl = "https://kanbanbackendpleasework.azurewebsites.net/";
 export const kanbanApi = axios.create({
   baseURL: baseUrl,
   withCredentials: true,
