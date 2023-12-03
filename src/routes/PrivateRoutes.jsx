55e9501f91f060dac2e1c7d3ac662f8fa8626ae0
Nicholas Gendron <52512014+nickgendron@users.noreply.github.com>
2023-12-03 13:48:26 -0600

    Update PrivateRoutes.jsx

--- src/routes/PrivateRoutes.jsx
+++ src/routes/PrivateRoutes.jsx
@@ -1,9 +1,33 @@
 import { Navigate, Outlet } from "react-router-dom";
 import { useSelector } from "react-redux";
-import React from "react";
+import { kanbanApi } from "../api/ApiInstance";
+import React, {useEffect} from "react";
+import { useDispatch } from "react-redux";
+import { setNotAuthenticated } from "../state/UserState/userStateSlice";
 export default function PrivateRoutes({ children }) {
+  const dispatch = useDispatch();
   const authenticated = useSelector((state) => state.userState.authenticated);
 
+  useEffect(() => {
+    const checkAuth = async () => {
+      try {
+        const response = await kanbanApi.get("/api/user/checkAuth");
+        console.log(response.status);
+
+        if (response.status === 401) {
+          // Handle unauthorized access, e.g., redirect to login page
+          // You can dispatch an action to update the Redux state accordingly
+        } else {
+          console.log(response);
+        }
+      } catch (error) {
+        dispatch(setNotAuthenticated());
+      }
+    };
+
+    checkAuth(); 
+  }, []); 
+// console.log(authStatus)
   if (authenticated) {
     return children;
   } else {
